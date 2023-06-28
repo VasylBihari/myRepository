d1b47f983de511c1c873635f526c9a063218f739
VasylBihari <128148257+VasylBihari@users.noreply.github.com>
2023-06-28 09:39:36 +0300

    sumDigits.java

--- sumDigits.java
+++ sumDigits.java
@@ -1,9 +1,9 @@
-﻿In this challenge, your task is to write a method, with the name sumDigits, that has a single parameter named number, of type int, and it should return an int.
+/*﻿In this challenge, your task is to write a method, with the name sumDigits, that has a single parameter named number, of type int, and it should return an int.
 The method should only take a number that is a positive number. 
 If a negative number is passed, it should return -1, meaning an invalid value was passed.
 The method should parse out each digit from the number, and sum the digits up.
-So if 125 is the value passed to the method, the code should sum each digit, in this case, 1 + 2 + 5, and return 8, as a value.
-_______________________________________________________________________________________________________________________________________
+So if 125 is the value passed to the method, the code should sum each digit, in this case, 1 + 2 + 5, and return 8, as a value.*/
+
 
 public class MyClass {
     public static void main(String args[]) {
